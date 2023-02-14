b74995026537468b59a2f4073092130c5a86dcab
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-02-14 20:55:12 +0100

    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework templatedasasdasddas
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>react homework</h2>
+      </div>
+    );
+  }
+}
