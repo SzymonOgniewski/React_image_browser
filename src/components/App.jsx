59730acf898ed35a32f687f6b0c23e328af46597
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-02-15 23:12:40 +0100

    added searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import { fetchImages } from './api/fetchImages';
-
+import './app.css';
+import { Searchbar } from './Searchbar/Searchbar';
 export class App extends React.PureComponent {
+  state = {
+    query: '',
+  };
   componentDidMount() {
     this.handleFetchImages();
   }
   handleFetchImages = async (searchQuery, page, perPage) => {
-    searchQuery = 'cat';
+    searchQuery = this.state.query;
     page = 1;
     perPage = 12;
     const response = await fetchImages(searchQuery);
-    console.log(response);
   };
 
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ query: this.state.query });
+    this.handleFetchImages();
+    this.setState({ query: '' });
+  };
   render() {
     return (
       <div>
-        <h2>react homework</h2>
+        <Searchbar
+          query={this.state.query}
+          handleQueryChange={this.handleChange}
+          handleFormSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import css from './searchbar.module.css';
 export class Searchbar extends Component {
-    
+  render() {
+    const { query, handleQueryChange, handleFormSubmit } = this.props;
+
+    return (
+      <header className={css.searchbar} onSubmit={handleFormSubmit}>
+        <form className={css.form}>
+          <button type="submit" className={css.button}>
+            <span className={css.buttonLabel}>Search</span>
+          </button>
+
+          <input
+            onChange={handleQueryChange}
+            className={css.input}
+            type="text"
+            name="query"
+            value={query}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
+
+Searchbar.propTypes = {
+  query: PropTypes.string,
+  handleQueryChange: PropTypes.func,
+  handleFormSubmit: PropTypes.func,
+};
